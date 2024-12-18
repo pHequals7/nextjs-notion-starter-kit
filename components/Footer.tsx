5a239a1e9cb4bb3721e7f99c9a9f2a37a81d6475
Pranav Hari <pranavhari@Pranavs-MacBook-Air.local>
2024-12-18 08:38:09 +0530

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react'
+import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
+import { FaLinkedin } from '@react-icons/all-files/fa/FaLinkedin'
+import { FaTwitter } from '@react-icons/all-files/fa/FaTwitter'
+import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
+import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import * as config from 'lib/config'
+
+import styles from './styles.module.css'
+
+
+export const Footer: React.FC<{
+  isDarkMode: boolean
+  toggleDarkMode: () => void
+}> = ({ isDarkMode, toggleDarkMode }) => {
+  const [hasMounted, setHasMounted] = React.useState(false)
+  const toggleDarkModeCb = React.useCallback(
+    (e) => {
+      e.preventDefault()
+      toggleDarkMode()
+    },
+    [toggleDarkMode]
+  )
+
+  React.useEffect(() => {
+    setHasMounted(true)
+  }, [])
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>© Copyright 2023 {config.author}</div>
+
+      {hasMounted ? (
+        <div className={styles.settings}>
+          <a
+            className={styles.toggleDarkMode}
+            onClick={toggleDarkModeCb}
+            title='Tottle dark mode'
+          >
+            {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
+          </a>
+        </div>
+      ) : null}
+
+      <div className={styles.social}>
+        {config.twitter && (
+          <a
+            className={styles.twitter}
+            href={`https://twitter.com/${config.twitter}`}
+            title={`Twitter @${config.twitter}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaTwitter />
+          </a>
+        )}
+
+        {config.github && (
+          <a
+            className={styles.github}
+            href={`https://github.com/${config.github}`}
+            title={`GitHub @${config.github}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaGithub />
+          </a>
+        )}
+
+        {config.linkedin && (
+          <a
+            className={styles.linkedin}
+            href={`https://www.linkedin.com/in/${config.linkedin}`}
+            title={`LinkedIn ${config.author}`}
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            <FaLinkedin />
+          </a>
+        )}
+      </div>
+    </footer>
+  )
+}
