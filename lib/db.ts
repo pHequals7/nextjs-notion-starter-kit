48964df042c446292ab267ce526f20bd580e8c53
Pranav Hari <39328505+pHequals7@users.noreply.github.com>
2025-01-02 21:35:39 +0530

    removed firestore imports

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,3 @@
-import * as firestore from '@google-cloud/firestore'
-import * as config from './config'
-
-export let db: firestore.Firestore = null
-export let images: firestore.CollectionReference = null
-
-if (config.isPreviewImageSupportEnabled) {
-  db = new firestore.Firestore({
-    projectId: config.googleProjectId,
-    credentials: config.googleApplicationCredentials
-  })
-
-  images = db.collection(config.firebaseCollectionImages)
-}
+// Simplified version without Firestore dependency
+export let db = null
+export let images = null
