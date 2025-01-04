77316797af83e995db5269dc3754957045cf80dd
Pranav Hari <39328505+pHequals7@users.noreply.github.com>
2025-01-04 06:53:34 +0530

    Update ErrorPage.tsx

--- components/ErrorPage.tsx
+++ components/ErrorPage.tsx
@@ -1,30 +1,32 @@
 import React from 'react'
 import Head from 'next/head'
+import Image from 'next/image'
 import { PageHead } from './PageHead'
-
 import styles from './styles.module.css'
 
 export const ErrorPage: React.FC<{ statusCode: number }> = ({ statusCode }) => {
   const title = 'Error'
-
   return (
     <>
       <PageHead />
-
       <Head>
         <meta property='og:site_name' content={title} />
         <meta property='og:title' content={title} />
-
         <title>{title}</title>
       </Head>
-
       <div className={styles.container}>
         <main className={styles.main}>
           <h1>Error Loading Page</h1>
-
           {statusCode && <p>Error code: {statusCode}</p>}
-
-          <img src='/error.png' alt='Error' className={styles.errorImage} />
+          <div className={styles.errorImage}>
+            <Image
+              src='/error.png'
+              alt='Error'
+              width={1216}
+              height={912}
+              priority
+            />
+          </div>
         </main>
       </div>
     </>
